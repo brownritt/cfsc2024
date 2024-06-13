9328036d79dbb1e4db004a8ab8feeb06264dd11a
Jason Ritt <54116515+brownritt@users.noreply.github.com>
2024-06-13 13:48:40 -0400

    Add: display script for EEG data

---
+++ examples/EEG_data/display_data.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Simple example script to display an associated data file.
+
+Part of the Carney Institute's 2024 Computational Fluency Short Course.
+
+To run from a terminal, type:
+
+python display_data.py
+
+The code must be in the same working directory as the data file EEG_S1_prestim.csv.
+"""
+
+#%% Import block
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+#%% Load data 
+
+# Assume the working directory contains the file
+datadir = "." 
+datafile = "EEG_S1_prestim.csv"
+
+datapath = os.path.join(datadir,datafile)
+
+df = pd.read_csv(datapath)
+
+#%% Display the data
+
+print("Close figure to quit")
+
+plt.figure()
+plt.plot(df["Time (sec)"], df["Electrode (uV)"])
+plt.xlabel("Time (sec)")
+plt.ylabel("Electrode (uV)")
+plt.title(f'Data in {datafile}')
+plt.show()
+
+# TODO: Auto detect header names
+# TODO: Make interactive figure
+
+
